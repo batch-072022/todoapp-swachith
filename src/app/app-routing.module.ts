e3bb6eaaa999d89b74318e582e72f3afc8d3db6f
Sai Swachith Koppuravuri <saiswachith.koppuravuri@valuemomentum.com>
2022-08-02 15:38:08 +0530

    commiting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+import { HomeComponent } from './home/home.component';
+import { ListtasksComponent } from './listtasks/listtasks.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [ {path:'home',component:HomeComponent},
+{path:'register',component:RegisterComponent},
+{path:'login',component:LoginComponent},
+{path:'addtask',component:AddTaskComponent},
+{path:'listtask',component:ListtasksComponent},
+{path:'edittask',component:EditTaskComponent},
+{path:'',redirectTo:'home',pathMatch:'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ListusersComponent } from './listusers/listusers.component';
+import { ViewprofileComponent } from './viewprofile/viewprofile.component';
+import { ListtasksComponent } from './listtasks/listtasks.component';
+import { ChangepasswordComponent } from './changepassword/changepassword.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    AddTaskComponent,
+    ForgotPasswordComponent,
+    ListusersComponent,
+    ViewprofileComponent,
+    ListtasksComponent,
+    ChangepasswordComponent,
+    EditTaskComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
